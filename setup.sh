8c8a0811a4a196f854d47f925736d24a46b4085d
ntrkd <networky@duck.com>
2026-04-03 03:11:27 +0000

    rename to setup.sh and include sops collection + age apt package

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 pipx install ansible --include-deps
 # include this in PATH even though it is injected into the ansible virtual env
 pipx inject ansible ansible-dev-tools --include-deps --include-apps
+
+ansible-galaxy collection install community.sops
+
+sudo apt install age
+# TODO: some way to install sops as well, perhaps in ansible directly?
